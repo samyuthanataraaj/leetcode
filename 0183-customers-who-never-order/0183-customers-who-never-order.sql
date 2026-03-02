39a84df39f68b87bc21ffab152214a97f69b0bde
samyuthanataraaj <146978796+samyuthanataraaj@users.noreply.github.com>
2026-03-02 12:37:26 +0530

    Time: 534 ms (59.95%), Space: 0B (100%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT c.name AS Customers
+FROM Customers c
+WHERE c.id NOT IN (
+    SELECT customerId
+    FROM Orders
+    WHERE customerId IS NOT NULL
+);
+;
