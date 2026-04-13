abebb98a2021b7a8bbaf14e33c8756bd9fb902f4
samyuthanataraaj <146978796+samyuthanataraaj@users.noreply.github.com>
2026-04-13 12:42:14 +0530

    Time: 2 ms (12.2%), Space: 46.9 MB (68.87%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -35,12 +35,12 @@ class Solution {
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.println(mat1[i][j]+" ");
-            }
-            System.out.println();
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         System.out.println(mat1[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat1[i][j]!=mat[i][j]){
