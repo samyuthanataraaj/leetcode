a21dc0bb66b67fd71cf0225e77ce1cf867603264
samyuthanataraaj <146978796+samyuthanataraaj@users.noreply.github.com>
2026-04-15 10:07:19 +0530

    Time: 1 ms (29.87%), Space: 46.7 MB (62.34%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,55 +1,67 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
-        int flag=-1;
-        for(int i=0;i<words.length;i++){
-            if(target.equals(words[i])){
-                flag=1;
-                break;
-            }
-        }
-        if(flag==-1){
-            return flag;
-        }
-        List<String> left=new ArrayList<>();
-        List<String> right=new ArrayList<>();
-        int k1=words.length;
-        int k2=startIndex;
+    //     int flag=-1;
+    //     for(int i=0;i<words.length;i++){
+    //         if(target.equals(words[i])){
+    //             flag=1;
+    //             break;
+    //         }
+    //     }
+    //     if(flag==-1){
+    //         return flag;
+    //     }
+    //     List<String> left=new ArrayList<>();
+    //     List<String> right=new ArrayList<>();
+    //     int k1=words.length;
+    //     int k2=startIndex;
        
-        for (int i = k2, count = 0; count < words.length; i++, count++) {
-            right.add(words[i]);
-            if (i == words.length - 1) {
-                i = -1;
-            }
-        }
+    //     for (int i = k2, count = 0; count < words.length; i++, count++) {
+    //         right.add(words[i]);
+    //         if (i == words.length - 1) {
+    //             i = -1;
+    //         }
+    //     }
 
-        int k4=startIndex;
-        int k5=0;
+    //     int k4=startIndex;
+    //     int k5=0;
         
-        for (int i = k4, count = 0; count < words.length; i--, count++) {
-            left.add(words[i]);
-            if (i == 0) {
-                i = words.length;
-            }
-        }
+    //     for (int i = k4, count = 0; count < words.length; i--, count++) {
+    //         left.add(words[i]);
+    //         if (i == 0) {
+    //             i = words.length;
+    //         }
+    //     }
 
-        System.out.println(left);
-        System.out.println(right);
-        int dist=Integer.MAX_VALUE;
-        for(int i=0;i<left.size();i++){
-            if(target.equals(left.get(i))){
-                if(dist>i){
-                    dist=i;
-                }
-            }
-        }
-        for(int i=0;i<right.size();i++){
-            if(target.equals(right.get(i))){
-                if(dist>i){
-                    dist=i;
-                }
-            }
-        }
-        return dist;
+    //     System.out.println(left);
+    //     System.out.println(right);
+    //     int dist=Integer.MAX_VALUE;
+    //     for(int i=0;i<left.size();i++){
+    //         if(target.equals(left.get(i))){
+    //             if(dist>i){
+    //                 dist=i;
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<right.size();i++){
+    //         if(target.equals(right.get(i))){
+    //             if(dist>i){
+    //                 dist=i;
+    //             }
+    //         }
+    //     }
+    //     return dist;
         
-    }
+    // }
+
+        int n = words.length;
+        for (int step = 0; step < n; step++) {
+            int forward = (startIndex + step) % n;
+            int backward = (startIndex - step + n) % n;
+            if (words[forward].equals(target) || words[backward].equals(target)) {
+                return step;
+            }
+        }
+        return -1;
+    
+}
 }
