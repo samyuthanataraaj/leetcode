6190627ba3a329e86ef13a99ed315cf276bc93a7
samyuthanataraaj <146978796+samyuthanataraaj@users.noreply.github.com>
2026-05-25 09:23:33 +0530

    Time: 13 ms (38.37%), Space: 53.3 MB (28.6%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+
+        int n = s.length();
+
+        if (s.charAt(n - 1) != '0')
+            return false;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+
+        int farthest = 0; // optimization
+
+        while (!q.isEmpty()) {
+            int i = q.poll();
+
+            if (i == n - 1) return true;
+
+            int start = Math.max(i + minJump, farthest);
+            int end = Math.min(i + maxJump, n - 1);
+
+            for (int j = start; j <= end; j++) {
+                if (s.charAt(j) == '0' && !visited[j]) {
+                    visited[j] = true;
+                    q.add(j);
+                }
+            }
+
+            farthest = end + 1;
+        }
+
+        return false;
+    }
+}
